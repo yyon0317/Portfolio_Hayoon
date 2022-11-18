8b9f6f57d29027cbf50ed76550d99b4e585f03cd
Ha Yoon <yhy8283@naver.com>
2022-11-18 19:41:39 +0900

    js 수정

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 jQuery(document).ready(function () {
+
+	/* 애니메이션 스크롤 이동 */
+	const animationMove = function (selector) {
+		// ① selector 매개변수로 이동할 대상 요소 노드 가져오기
+		const targetEl = document.querySelector(selector);
+		// ② 현재 브라우저의 스크롤 정보(y 값)
+		const browserScrollY = window.pageYOffset;
+		// ③ 이동할 대상의 위치(y 값)
+		const targetScorllY = targetEl.getBoundingClientRect().top + browserScrollY;
+		window.scrollTo({
+			top: targetScorllY,
+			behavior: 'smooth'
+		});
+	};
+	// 스크롤 이벤트 연결하기
+	const scollMoveEl = document.querySelectorAll("[data-animation-scroll='true']");
+	for (let i = 0; i < scollMoveEl.length; i++) {
+		scollMoveEl[i].addEventListener('click', function (e) {
+			const target = this.dataset.target;
+			animationMove(target);
+		});
+	}
+
 	
 	//menu 자바스크립트
 	document.querySelector('.book1').addEventListener('click',function(){
